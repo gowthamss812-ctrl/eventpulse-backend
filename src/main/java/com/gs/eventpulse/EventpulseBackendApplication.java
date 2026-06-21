5ff55a26ce72ec5d773d65825dc898af63275443
Gowtham S <gowthamss812@gmail.com>
2026-06-21 12:41:39 -0400

    Initial Spring Boot project setup with Java 21

---
+++ src/main/java/com/gs/eventpulse/EventpulseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gs.eventpulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventpulseBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventpulseBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gs/eventpulse/EventpulseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gs.eventpulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventpulseBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
